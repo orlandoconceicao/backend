fff5888bbbe91a3f8210edb7929ae427535dcc06
orlandoconceicao <orlandoconceicao94@gmail.com>
2026-07-08 16:32:50 -0400

    docs: remove contato e sobre

--- Projetos-para-estudo/pagina-de-receitas/recipes_page/views.py
+++ Projetos-para-estudo/pagina-de-receitas/recipes_page/views.py
@@ -2,10 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
-
-def contato(request):
-    return HttpResponse("Esta é a página de contato!")
-
-def sobre(request):
-    return HttpResponse("Esta é a página sobre!")
+    return render(request, 'home.html', status=200)
