e716ad8989f4e3fe9caffe9730a13ac0b9884874
orlandoconceicao <orlandoconceicao94@gmail.com>
2026-07-25 23:34:49 -0400

    perf: coloca conteúdo de exemplo e nome nas paginas

--- projetos/Projeto receitas/pagina-de-receitas/recipes_page/urls.py
+++ projetos/Projeto receitas/pagina-de-receitas/recipes_page/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from recipes_page import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('recipes/<int:id>/', views.recipes, name='recipes-view'),
 ]
--- projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
+++ projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'page/home.html', status=200)
+from utils.recipes.factory import make_recipe
 
+def home(request): return render(request, 'page/home.html', context={'recipes': [make_recipe() for _ in range(10)],})
+
+def recipes(request, id): return render(request, 'page/recipe-view.html', context={'recipe': make_recipe(),})
