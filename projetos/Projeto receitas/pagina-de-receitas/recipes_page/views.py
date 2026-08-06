e89bcf352c31c91d1489fcec7547a089002365dc
orlando <orlandoconceicao94@gmail.com>
2026-08-06 18:32:09 -0400

    perf: remove importações desnecessarias

--- projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
+++ projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
@@ -4,8 +4,6 @@ from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
-from django.http import Http404
-
 def home(request): 
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'page/home.html', context={
@@ -13,11 +11,8 @@ def home(request):
     })
     
 def category(request, category_id): 
-    # recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    #if not recipes:    
-    #   raise Http404("Not Found")
     recipes = get_list_or_404(Recipe.objects.filter, category__id=category_id, is_published=True).order_by('-id')
-    
+
     return render(request, 'page/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | ',
