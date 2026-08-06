8857fbf9998da548f4d11272bbb3e2d651c9b88e
orlando <orlandoconceicao94@gmail.com>
2026-08-06 18:30:45 -0400

    perf: faz que as receitas peguem informações do banco de dados

--- projetos/Projeto receitas/pagina-de-receitas/recipes_page/models.py
+++ projetos/Projeto receitas/pagina-de-receitas/recipes_page/models.py
@@ -21,8 +21,9 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) 
 
     def __str__(self):
         return self.title
+    
--- projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
+++ projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 from utils.recipes.factory import make_recipe
 
-def home(request): return render(request, 'page/home.html', context={
-    'recipes': [make_recipe() for _ in range(10)],
-    })
+from .models import Recipe
 
+from django.http import Http404
+
+def home(request): 
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'page/home.html', context={
+        'recipes': recipes,
+    })
+    
+def category(request, category_id): 
+    # recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    #if not recipes:    
+    #   raise Http404("Not Found")
+    recipes = get_list_or_404(Recipe.objects.filter, category__id=category_id, is_published=True).order_by('-id')
+    
+    return render(request, 'page/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | ',
+    })
+    
 def recipes(request, id): return render(request, 'page/recipe-view.html', context={
     'recipe': make_recipe(),
     'is_detail_page': True,
     })
+
+
+
+
