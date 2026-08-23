c5abf183975209165e44edadd16a31c841dd0673
orlando <orlandoconceicao94@gmail.com>
2026-08-23 12:54:29 -0400

    perf: permite que as receitas usam html para modo de preparo

--- projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
+++ projetos/Projeto receitas/pagina-de-receitas/recipes_page/views.py
@@ -4,25 +4,40 @@ from utils.recipes.factory import make_recipe
 
 from .models import Recipe
 
-def home(request): 
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
-    return render(request, 'page/home.html', context={
-        'recipes': recipes,
-    })
-    
-def category(request, category_id): 
-    recipes = get_list_or_404(Recipe.objects.filter, category__id=category_id, is_published=True).order_by('-id')
 
-    return render(request, 'page/category.html', context={
-        'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category | ',
-    })
-    
-def recipes(request, id): return render(request, 'page/recipe-view.html', context={
-    'recipe': make_recipe(),
-    'is_detail_page': True,
-    })
+def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
+    return render(
+        request,
+        "page/home.html",
+        context={
+            "recipes": recipes,
+        },
+    )
 
 
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter, category__id=category_id, is_published=True
+    ).order_by("-id")
+
+    return render(
+        request,
+        "page/category.html",
+        context={
+            "recipes": recipes,
+            "title": f"{recipes[0].category.name} - Category | ",
+        },
+    )
 
 
+def recipes(request, id):
+    recipe = Recipe.objects.filter(id=id, is_published=True).first()
+    return render(
+        request,
+        "page/recipe-view.html",
+        context={
+            "recipe": recipe,
+            "is_detail_page": True,
+        },
+    )
